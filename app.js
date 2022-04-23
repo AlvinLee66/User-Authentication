c1bcdb4e6572e5d14f619d7c0999b95dfee40ee2
AlvinLee66 <ewqaz001@gmail.com>
2022-04-23 14:06:30 +0800

    feat: modify index.hbs and add users data

--- app.js
+++ app.js
@@ -3,11 +3,45 @@ const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const body = req.body
+  console.log(body)
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
